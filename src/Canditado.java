081a5b84755a9a7b188288b5b96b9c87318d0656
raphael101216 <raphaelrock_8@hotmail.com>
2020-05-13 20:22:15 -0300

    Leitura da escolaridade

---
+++ src/Canditado.java
@@ -0,0 +1,31 @@
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Canditado {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		char viajar, cnh;
+		int escolaridade, pontosEscola, pontosExperie, anosExperie;
+		
+		
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.print("Digite uma opção: ");
+		escolaridade = sc.nextInt();
+		
+		
+		
+
+	}
+
+}
